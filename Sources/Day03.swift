0c6f93100d39f1a82d32774b4ae0dc07d22e9bbe
Robbie Murray <rob@mur.org.uk>
2025-12-04 00:53:11 +0000

    Add Day03 implementation and update tests with new data

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -6,20 +6,53 @@ struct Day03: AdventDay {
 
   // Splits input data into its component parts and convert from string.
   var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+    data.split(separator: "\n").map {
+      $0.map{Int(String($0))!}
     }
   }
 
+  func maxJolts <T: RandomAccessCollection> (_ bank: T) -> Int
+  where T.Element == Int, T.Index == Int{
+    let maxFirst = bank.dropLast().max()
+    let firstPosCandidates = bank.dropLast().indices(where: {$0 == maxFirst})
+    var candidates: [Int] = []
+    for range in firstPosCandidates.ranges {
+      for i in range {
+        let m = bank[(i+1)...].max()!
+        candidates.append(bank[i]*10+m)
+      }
+    }
+    return candidates.max()!
+  }
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
+    entities.map{
+      maxJolts($0)
+    }.reduce(0, +)
   }
+// attempted to use array slices for the recursion but it fails due to the reliance on integer indices
+  func maxJolts <T: RandomAccessCollection> (of bank: T, size: Int) -> Int
+  where T.Element == Int, T.Index == Int {
+    if size == 2 {
+      return maxJolts(bank)
+    }
+    let maxFirst = bank.dropLast(size-1).max()
+    let firstPosCandidates = bank.dropLast(size-1).indices(where: {$0 == maxFirst})
+    var candidates: [Int] = []
+    for range in firstPosCandidates.ranges {
+      for i in range {
+        candidates.append(intPow(10, size-1) * bank[i]
+          + maxJolts(of: Array(bank[(i+1)...]), size: size-1))
+      }
+    }
+    return candidates.max()!
+  }
+
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+    entities.map{
+      maxJolts(of: $0, size: 12)
+    }.reduce(0, +)
   }
 }
