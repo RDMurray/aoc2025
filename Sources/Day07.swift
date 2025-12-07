acde2139df9a8148be98e9995d136e2572fba18f
Robbie Murray <rob@mur.org.uk>
2025-12-07 23:59:37 +0000

    complete day 7

--- Sources/Day07.swift
+++ Sources/Day07.swift
@@ -1,25 +1,67 @@
 import Algorithms
 
+enum Node {
+  case space
+  case splitter
+  case source
+}
+
 struct Day07: AdventDay {
   // Save your data in a corresponding text file in the `Data` directory.
   var data: String
 
   // Splits input data into its component parts and convert from string.
-  var entities: [[Int]] {
-    data.split(separator: "\n\n").map {
-      $0.split(separator: "\n").compactMap { Int($0) }
+  var entities: [[Node]] {
+    data.split(separator: "\n").map {
+      $0.map {
+        switch $0 {
+        case "S": .source
+        case ".": .space
+        case "^": .splitter
+        default: .space
+        }
+      }
     }
   }
 
   // Replace this with your solution for the first part of the day's challenge.
   func part1() -> Any {
-    // Calculate the sum of the first set of input data
-    entities.first?.reduce(0, +) ?? 0
+    var splitCount = 0
+    var beams = [Bool](repeating: false, count: entities.first!.count)
+    for row in entities {
+      for (index, node) in row.enumerated() {
+        switch node {
+        case .space: break
+        case .source: beams[index] = true
+        case .splitter:
+          if beams[index] {
+            splitCount += 1
+            beams[index - 1] = true
+            beams[index] = false
+            beams[index + 1] = true  //will crash with splitters in first or last column.
+          }
+        }
+      }
+
+    }
+    return splitCount
   }
 
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Any {
-    // Sum the maximum entries in each set of data
-    entities.map { $0.max() ?? 0 }.reduce(0, +)
+    var timelineState = [Int](repeating: 0, count: entities.first!.count)  // slides down the data keeping track of the timelines that get to each node
+    for row in entities {
+      for (index, node) in row.enumerated() {
+        switch node {
+        case .source: timelineState[index] = 1
+        case .space: break
+        case .splitter:
+          timelineState[index - 1] += timelineState[index]
+          timelineState[index + 1] += timelineState[index]
+          timelineState[index] = 0
+        }
+      }
+    }
+    return timelineState.reduce(0, +)
   }
 }
--- Tests/Day07.swift
+++ Tests/Day07.swift
@@ -7,30 +7,31 @@ import Testing
 struct Day07Tests {
   // Smoke test data provided in the challenge question
   let testData = """
-    1000
-    2000
-    3000
-
-    4000
-
-    5000
-    6000
-
-    7000
-    8000
-    9000
-
-    10000
-
+    .......S.......
+    ...............
+    .......^.......
+    ...............
+    ......^.^......
+    ...............
+    .....^.^.^.....
+    ...............
+    ....^.^...^....
+    ...............
+    ...^.^...^.^...
+    ...............
+    ..^...^.....^..
+    ...............
+    .^.^.^.^.^...^.
+    ...............
     """
 
   @Test func testPart1() async throws {
     let challenge = Day07(data: testData)
-    #expect(String(describing: challenge.part1()) == "6000")
+    #expect(String(describing: challenge.part1()) == "21")
   }
 
   @Test func testPart2() async throws {
     let challenge = Day07(data: testData)
-    #expect(String(describing: challenge.part2()) == "32000")
+    #expect(String(describing: challenge.part2()) == "40")
   }
 }
